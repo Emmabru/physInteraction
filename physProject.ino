06190253434afc80da204310c4abac0a569a69db
Emmabru <emmabru@kth.se>
2018-02-26 09:56:59 +0100

    distans fixat, lampa blinkar så många gånger som bokens nummer( R=100 är bok 4, R=150 är bok 3 osv). Verkar som R måste kalibreras varje gång :s

--- physProject.ino
+++ physProject.ino
@@ -12,11 +12,20 @@ const int echoPin = 10;
 const int trigPin2 = 11;
 const int echoPin2 = 12;
 const int lampa2 = 2;
+
 // defines variables
 long duration;
 int distance;
 long duration2;
 int distance2;
+
+int blinks = 0;
+int i;
+int blinkDelay = 300;
+
+int callDist = 20;
+int talkDist = 5;
+int patience= 5000;
 
 
 //resistance
@@ -46,19 +55,7 @@ duration = pulseIn(echoPin, HIGH);
 
 // Calculating the distance
 distance= duration*0.034/2;
-
-// Prints the distance on the Serial Monitor
-if (distance < 300) {
- // Serial.println(" HEY left BUDDY! COME ON OVER!");
   
-  if (distance < 150) {
-   // Serial.println(" WANNA READ A BOOK?!?!");
-  }  
-}
-//Serial.print("Distance left: ");
-//Serial.println(distance);
-//delay(2000);
-
 // Clears the trigPin2    **********************************
 digitalWrite(trigPin2, LOW);
 delayMicroseconds(2);
@@ -73,98 +70,137 @@ duration2 = pulseIn(echoPin2, HIGH);
 
 // Calculating the distance
 distance2= duration2*0.034/2;
-
-// Prints the distance on the Serial Monitor
-//Serial.print("Distance Right: ");
-//Serial.println(distance2);
-//delay(3000);
-
-//lights                  **********************************
-//digitalWrite(lampa2, );   // turn the LED on (HIGH is the voltage level)
-//delay(1000);                       // wait for a second
-digitalWrite(lampa2, LOW); 
-
-
-// Books                   *********************************
-
-//Resistance
-delay(1500);
-sensorValue = analogRead(0);
   
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (3.5 / 1024.0);
-
-  float I = voltage / 100;
-  float VRx = 3.5 - voltage;
-  float Rx = VRx / I;
-  Rx = (3.5 - voltage) / I;
-
-  // print out the value you read:
-  //Serial.println("Vilken Bok: ");
-  /*if(Rx > 1700)
-  {
-   Serial.println("#S|LILLBOKEN|[]#");
-
-    }
-else if(Rx > 1500)
-      {
-    Serial.println("#S|STORBOKEN|[]#");
-
-    }*/
-  Serial.println(" "); 
-  Serial.print(Rx);
-  Serial.println(" Ohms");
-  /*Serial.print("Bok 1: ");
-  Serial.println(analogRead(1));
-  Serial.print("Bok 2: ");
-  Serial.println(analogRead(2));
-  Serial.print("Bok 3: ");
-  Serial.println(analogRead(3));
-  Serial.print("Bok 4: ");
-  Serial.println(analogRead(4));
-  //  Serial.print("Resistance 4: ");
-  //Serial.print(analogRead(4));
- // Serial.println(" Ohms");
-  delay(2000);*/
-
-//What book was taken   **************************************
-if (Rx < 175) {
-  Serial.print("No book was taken    --> ");
-  Serial.println(Rx);
-
-} else if (Rx > 174){   // Delay to stabalize values
-    delay(1000);
-    
-    if (175 < Rx && Rx < 184) {
-      Serial.print("Bok 1 was taken    --> ");
-      Serial.print(Rx);
+// Prints the distance on the Serial Monitor
+if ((distance || distance2 < callDist)){
+      Serial.print("Hey Come here, read a book!     --->     ");
+      Serial.print(distance);
       Serial.print("    ");
-      Serial.println(analogRead(1));
-    
-    } else if (183 < Rx && Rx < 187) {
-      Serial.print("Bok 2 was taken    --> ");
-      Serial.print(Rx);
+      Serial.println(distance2);
+      delay(patience);
+  
+      if (distance || distance2 < talkDist) {
+        Serial.print("Fun! Grab a book and I'll talk about it! ");
+      Serial.print(distance);
       Serial.print("    ");
-      Serial.println(analogRead(2));
+      Serial.println(distance2);
+      delay(patience);
+
+      //        LIGHTS                 **********************************           LIGHTS
+      for(i = 0; i < blinks ; i++){
+        digitalWrite(lampa2, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);                       // wait for a second
+        digitalWrite(lampa2, LOW); 
+      }
       
-    } else if (186 < Rx && Rx < 205) {
-      Serial.print("Bok 3 was taken    --> ");
-      Serial.print(Rx);
-      Serial.print("    ");
-      Serial.println(analogRead(3));
+      //        BOOKS                   *********************************           BOOKS
       
-    } else if (199 < Rx) {
-      Serial.print("Bok 4 was taken    --> ");
-      Serial.print(Rx);
-      Serial.print("    ");
-      Serial.println(analogRead(4));
-    }
-}
-/*
-  start 165
-1   (100) 216-219   bok 4
-2   (150) 195-201 bok 3-4 mer 3  
-3   (180) 181-187,6 bok 3
-4  (220) 182,87 bok 2??
-  */
+      //timer for loop, until it looks at distance again
+      uint32_t period = 5 * 10000L;       // 1 minutes
+
+      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
+
+          //Resistance
+          delay(1500);
+          sensorValue = analogRead(0);
+            
+            // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+            float voltage = sensorValue * (3.5 / 1024.0);
+          
+            float I = voltage / 100;
+            float VRx = 3.5 - voltage;
+            float Rx = VRx / I;
+            Rx = (3.5 - voltage) / I;
+          
+          /*
+            Serial.println(" "); 
+            Serial.print(Rx);
+            Serial.println(" Ohms");
+            Serial.print("Bok 1: ");
+            Serial.println(analogRead(1));
+            Serial.print("Bok 2: ");
+            Serial.println(analogRead(2));
+            Serial.print("Bok 3: ");
+            Serial.println(analogRead(3));
+            Serial.print("Bok 4: ");
+            Serial.println(analogRead(4));
+            //  Serial.print("Resistance 4: ");
+            //Serial.print(analogRead(4));
+           // Serial.println(" Ohms");
+            delay(2000);*/
+          
+          //What book was taken   **************************************
+          if (Rx < 175) {
+            Serial.print("No book was taken    --> ");
+            Serial.println(Rx);
+            delay(1000);
+          
+          } else if (Rx > 174){   // Delay to stabalize values
+              delay(1000);
+              
+              if (177 < Rx && Rx < 184) {
+                Serial.print("Bok 1 was taken    --> ");
+                Serial.print(Rx);
+                Serial.print("    ");
+                Serial.println(analogRead(1));
+                tStart = millis();       // restart timer
+                blinks = 1;
+                for(i = 0; i < blinks ; i++){
+                  digitalWrite(lampa2, HIGH);   // turn the LED on (HIGH is the voltage level)
+                  delay(blinkDelay/blinks);                       // wait for a second
+                  digitalWrite(lampa2, LOW); 
+                }
+              
+              } else if (183 < Rx && Rx < 187) {
+                Serial.print("Bok 2 was taken    --> ");
+                Serial.print(Rx);
+                Serial.print("    ");
+                Serial.println(analogRead(2));
+                tStart = millis();       // restart timer
+                blinks = 2;
+                for(i = 0; i < blinks ; i++){
+                  digitalWrite(lampa2, HIGH);   // turn the LED on (HIGH is the voltage level)
+                  delay(blinkDelay/blinks);                       // wait for a second
+                  digitalWrite(lampa2, LOW); 
+                }
+                
+              } else if (186 < Rx && Rx < 205) {
+                Serial.print("Bok 3 was taken    --> ");
+                Serial.print(Rx);
+                Serial.print("    ");
+                Serial.println(analogRead(3));
+                tStart = millis();       // restart timer
+                blinks = 3;
+                for(i = 0; i < blinks ; i++){
+                  digitalWrite(lampa2, HIGH);   // turn the LED on (HIGH is the voltage level)
+                  delay(blinkDelay/blinks);                       // wait for a second
+                  digitalWrite(lampa2, LOW); 
+                }
+                
+              } else if (199 < Rx) {
+                Serial.print("Bok 4 was taken    --> ");
+                Serial.print(Rx);
+                Serial.print("    ");
+                Serial.println(analogRead(4));
+                tStart = millis();       // restart timer
+                blinks = 4;
+                for(i = 0; i < blinks ; i++){
+                  digitalWrite(lampa2, HIGH);   // turn the LED on (HIGH is the voltage level)
+                  delay(blinkDelay/blinks);                       // wait for a second
+                  digitalWrite(lampa2, LOW); 
+                }
+              }
+          }
+      }
+  /*
+    start 165                         170.18
+  1   (100) 216-219   bok 4           210-211.30      478-480
+  2   (150) 185 bok 3-4 mer 3         192-194.25      347-349
+  3   (180) 181-187,6 bok 3           194-198.54      340-346
+  4  (220) 182,87 bok 2??             190.81-201      351-358
+    */
+     }
+      Serial.println(" ");  
+  }
+
 }
